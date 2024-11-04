3970f024ae7bb815ec1b27c6d0d49862ab96ad25
syihab achmad <syihabachmad0@gmail.com>
2024-11-04 21:02:43 +0700

    initial commit

---
+++ src/IDlocalDateToDate.js
@@ -0,0 +1,27 @@
+function replaceDateToSystemFormat(indonesianDate) {
+  if (new Date(indonesianDate) != "Invalid Date") {
+    return new Date(indonesianDate);
+  } else {
+    const month = [
+      "januari",
+      "februari",
+      "maret",
+      "april",
+      "mei",
+      "juni",
+      "juli",
+      "agustus",
+      "september",
+      "oktober",
+      "november",
+      "desember",
+    ];
+
+    indonesianDate = indonesianDate.split(" ");
+    const day = indonesianDate[0];
+    const monthIndex = month.indexOf(indonesianDate[1].toLowerCase());
+    const year = indonesianDate[2];
+
+    return new Date(year, monthIndex, day);
+  }
+}
